3e8a8303c520165b581c167f97ecdfbe79b02744
Paweł Szudrowicz <pablo.szudrowicz@gmail.com>
2017-06-09 18:29:32 +0200

    Add Auditor and Agent functionality

--- openMPI/main.cpp
+++ openMPI/main.cpp
@@ -7,9 +7,27 @@
 //
 
 #include "mpi.h"
+#include <iostream>
+#include "stdlib.h"
 #include <stdio.h>
+#include <string.h>
+using namespace std;
+
+enum ProcesType {
+    Agent,
+    University,
+    Auditor
+};
 
 int main(int argc, char** argv) {
+    
+    ProcesType procesType;
+    int numberOfAgents = 2;
+    int numberOfUniv = 1;
+    int numberOfAudit = 1;
+    int S = 100;
+    
+    setbuf(stdout, NULL);
     // Initialize the MPI environment
     MPI_Init(NULL, NULL);
     
@@ -20,27 +38,113 @@ int main(int argc, char** argv) {
     // Get the rank of the process
     int world_rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+
     
-//    // Get the name of the processor
-//    char processor_name[MPI_MAX_PROCESSOR_NAME];
-//    int name_len;
-//    MPI_Get_processor_name(processor_name, &name_len);
-    
-    int dataToSend = 0;
-    if(world_rank == 0) {
-        MPI_Send(&dataToSend, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
-        
-        MPI_Recv(&dataToSend, 1, MPI_INT, world_size-1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        printf("Received %d,from %d, I'm %d \n", dataToSend, 3, world_rank);
+    if(numberOfAgents + numberOfAudit + numberOfUniv != world_size) {
+        printf("Number of processes not correct!\n");
+        MPI_Abort(MPI_COMM_WORLD, 0);
     }
-    else {
+    
+    if(world_rank < numberOfAgents) {
+        procesType = ProcesType::Agent;
+    } else if (world_rank < numberOfAgents + numberOfUniv) {
+        procesType = ProcesType::University;
+    } else {
+        procesType = ProcesType::Auditor;
+    }
+    
+    
+    if(procesType == Agent) {
         
-        int receiveFrom = world_rank-1;
+        while(true) {
+            srand((unsigned int)time(NULL));
+            int numberOfStudents = rand() % S + 1;
+            for(int i = 0; i<numberOfUniv ; i++) {
+                MPI_Send(&numberOfStudents, 1, MPI_INT, numberOfAgents + i, 0, MPI_COMM_WORLD);
+                printf("Agent %d: Sent numberOfStudent = %d to University %d\n", world_rank, numberOfStudents, numberOfAgents + i);
+            }
+            
+            char message[numberOfUniv][8];
+            for(int k = 0; k<numberOfUniv ; k++) {
+                MPI_Recv(message[k], 8, MPI_CHAR, MPI_ANY_SOURCE, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                printf("Agent %d: Received message = %s from University\n", world_rank, message[k]);
+            }
+            
+            
+        }
         
-        MPI_Recv(&dataToSend, 1, MPI_INT, receiveFrom, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        printf("Received %d,from %d, I'm %d \n", dataToSend, receiveFrom, world_rank);
-        dataToSend += 1;
-        MPI_Send(&dataToSend, 1, MPI_INT, (world_rank+1)%world_size, 0, MPI_COMM_WORLD);
+    }
+    else if(procesType == University) {
+        
+        char message[8];
+        
+        int totalStudents=0;
+        while(true) {
+            int receivedStudents;
+            for(int i=0; i<numberOfAgents; i++) {
+                MPI_Recv(&receivedStudents, 1, MPI_INT, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                printf("University %d: Received students = %d from Agent %d\n", world_rank, receivedStudents,i);
+                totalStudents+=receivedStudents;
+            }
+            
+            
+            sprintf(message, "%d:%d:", world_rank, S);
+            for(int i = 0; i<numberOfAgents ; i++) {
+                MPI_Send(message, 8, MPI_CHAR, i, 1, MPI_COMM_WORLD);
+                printf("University %d: Send message = %s to Agent %d\n", world_rank, message, i);
+            }
+            for(int i = 0; i<numberOfAudit ; i++) {
+                MPI_Send(message, 8, MPI_CHAR, numberOfAgents + numberOfUniv + i, 2, MPI_COMM_WORLD);
+                printf("University %d: Send message = %s to Auditor %d\n", world_rank, message, numberOfAgents + numberOfUniv + i);
+            }
+        }
+        
+    } else if(procesType == Auditor) {
+        
+        while(true) {
+            char message[numberOfUniv][8];
+            for(int k = 0; k<numberOfUniv ; k++) {
+                MPI_Recv(message[k], 8, MPI_CHAR, MPI_ANY_SOURCE, 2, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                printf("Auditor %d: Received message = %s from University\n", world_rank, message[k]);
+            }
+            int punish[numberOfUniv] ;
+            for(int i =0 ; i< numberOfUniv ; i++) {
+                punish[i] = 0;
+            }
+            
+            for(int z = 0; z<numberOfUniv/2 ; z++) {
+                int univID = rand() % numberOfUniv;
+                std::string str = string(message[univID]);
+
+                std::string delimiter = ":";
+                size_t pos = 0;
+                std::string token[2];
+                int num = 0;
+                while ((pos = str.find(delimiter)) != std::string::npos) {
+                    
+                    token[num] = str.substr(0, pos);
+                    str.erase(0, pos + delimiter.length());
+                    num++;
+                }
+                
+                
+                int idProc = atoi(token[0].c_str());
+                int studentToFill = atoi(token[1].c_str());
+                
+                if(studentToFill == 0) {
+                    //? array index -1
+                    punish[numberOfAgents + numberOfUniv - idProc] = 1;
+                } else {
+                    punish[numberOfAgents + numberOfUniv - idProc] = 0;
+                }
+            }
+            
+            for(int i = 0 ;i< numberOfUniv; i++) {
+                MPI_Send(&punish[i], 1, MPI_INT, numberOfAgents + i, 3, MPI_COMM_WORLD);
+                printf("Auditor %d: Send punish = %d to University %d\n", world_rank, punish[i], numberOfAgents + i);
+            }
+        }
+        
     }
     
     
